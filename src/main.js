0b6c02b07c8bcbab1757ffc0b9e6afeec061e10a
Mukund <mukundjain2803@gmail.com>
2025-05-02 00:25:07 +0530

    fix some bugs

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
 import './firebase.js';   // sets up `auth`
 import './signin.js';     // your sign-in logic
 import './components/signuppage/signup.js';
+import './components/homepage/sript.js';
+import './components/PersonalProblemSection/script.js';
+import './components/PsychologicalProblemSection/script.js';
+import './components/WorkProblemSection/script.js';
